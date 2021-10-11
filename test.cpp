5f61ad4bf76dafa53c7d40ef883856325a337f90
Stackingrule <messageswitching@gmail.com>
2021-10-11 23:05:36 +0800

    init project

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include "toyjson.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Init ToyJSON Project!" << endl;
+}
---
+++ toyjson.h
@@ -0,0 +1,10 @@
+#ifndef TOYJSON_TOYJSON_H
+#define TOYJSON_TOYJSON_H
+
+
+class toyjson {
+
+};
+
+
+#endif //TOYJSON_TOYJSON_H
